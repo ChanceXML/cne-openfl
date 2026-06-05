8196422976cc26ff4e8eb0eec1185c815ea7c128
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-06-05 11:22:29 -0700

    testing

--- src/openfl/display/BitmapData.hx
+++ src/openfl/display/BitmapData.hx
@@ -3221,6 +3221,8 @@ class BitmapData implements IBitmapDrawable
 
 			context.hello();
 
+			trace(context.helloVar);
+
 			@:privateAccess context.__clear(useScissor, color.r / 0xFF, color.g / 0xFF, color.b / 0xFF, transparent ? color.a / 0xFF : 1, 0, 0, Context3DClearMask.COLOR);
 
 			if (useScissor)
